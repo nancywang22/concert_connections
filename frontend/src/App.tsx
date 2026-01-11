dbf886d733cdebd3b4fe7aa83b63e683ca61a5bb
Nancy Wang <nanwan@ucdavis.edu>
2026-01-11 01:51:09 -0800

    minor fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,6 @@ const App: React.FC = () => {
         {/* Page content */}
         <main className="p-4">
           <Routes>
-            
             <Route path="/" element={<MainPage />} />
             <Route path="/auth" element={<AuthPage />} />
             <Route path="/log" element={<LogConcertPage />} />
